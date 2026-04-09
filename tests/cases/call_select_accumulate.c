e19ef3725f0de3624387d526e48097846d18176f
Iris Shi <0.0@owo.li>
2026-04-09 19:10:37 +0800

    generated some test cases x2

---
+++ tests/cases/call_select_accumulate.c
@@ -0,0 +1,18 @@
+static volatile int state = 37;
+
+__attribute__((noinline)) static int f(int a) { return a * 3 - 5; }
+__attribute__((noinline)) static int g(int a) { return (a ^ 0x55) + 7; }
+
+int _start(void) {
+  int acc = state;
+
+  for (int i = 0; i < 4; i++) {
+    int a = f(acc + i);
+    int b = g(acc - i);
+    int c = (a > b) ? (a - b) : (b - a);
+    acc = ((i & 1) ? a : b) ^ c ^ acc;
+  }
+
+  state = acc;
+  return acc;
+}
---
+++ tests/cases/table_memory_stride_long.c
@@ -0,0 +1,43 @@
+typedef int (*fn_t)(int, int);
+
+static volatile unsigned seed = 0x00c0ffeeu;
+
+__attribute__((noinline)) static int add2(int a, int b) { return a + b; }
+__attribute__((noinline)) static int sub2(int a, int b) { return a - b; }
+__attribute__((noinline)) static int mul2(int a, int b) { return a * b; }
+__attribute__((noinline)) static int xor2(int a, int b) { return a ^ b; }
+
+int _start(void) {
+  fn_t table[4] = {add2, sub2, mul2, xor2};
+  volatile unsigned char *buf = (volatile unsigned char *)(unsigned)0x340;
+  unsigned acc = seed;
+
+  for (unsigned i = 0; i < 12u; i++) {
+    unsigned a = acc ^ (i * 13u);
+    unsigned b = ((acc >> ((i & 3u) + 1u)) | 1u) + i;
+    int v = table[(a + b) & 3u]((int)a, (int)b);
+
+    buf[i & 7u] = (unsigned char)v;
+    int s = (int)(signed char)buf[i & 7u];
+
+    switch ((v + (int)i) & 3) {
+      case 0:
+        acc += (unsigned)(v ^ s);
+        break;
+      case 1:
+        acc ^= (unsigned)(v - s);
+        continue;
+      case 2:
+        acc -= (unsigned)(v + s);
+        break;
+      default:
+        acc += (unsigned)((v << 1) ^ (int)i);
+        break;
+    }
+
+    acc = (acc << 3) ^ (acc >> 2) ^ (unsigned)(s + (int)i);
+  }
+
+  seed = acc;
+  return (int)acc;
+}
