f79583b58ba6f6ce8d979f3931c21947ad950f3f
Iris Shi <0.0@owo.li>
2026-04-09 22:53:24 +0800

    const shift bit inlining

---
+++ tests/cases/shift_rotate_const.c
@@ -0,0 +1,14 @@
+__attribute((optnone))
+int _start(void) {
+  volatile unsigned int x = 0x91a2b3c4u;
+  volatile int sx = (int)x;
+
+  unsigned int shl = x << 5;
+  unsigned int shr_u = x >> 5;
+  unsigned int shr_s = (unsigned int)(sx >> 5);
+  unsigned int rotl = (x << 5) | (x >> 27);
+  unsigned int rotr = (x >> 5) | (x << 27);
+  unsigned int div_pow2 = x / 8u;
+
+  return (int)(shl ^ shr_u ^ shr_s ^ rotl ^ rotr ^ div_pow2);
+}
